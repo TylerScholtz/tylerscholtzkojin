979a92e45180c7f9655948f64eb3018771fea36c
TylerScholtz <139673645+TylerScholtz@users.noreply.github.com>
2023-07-21 17:07:00 +0900

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-
-var navbar = document.getElementById('navbar');
-var hamburger = document.getElementById('hamburger');
-
-hamburger.addEventListener('click', function() {
-    navbar.style.display = navbar.style.display === 'none' ? 'flex' : 'none';
-});
-
